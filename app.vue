dd9e2607a25e072202517b9adc77b8011bf84edc
tomohiro1024 <gongyezhihong732@gmail.com>
2023-08-28 14:58:49 +0900

    大体完成

--- app.vue
+++ app.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>TODOリスト</h1>
+    <form @submit.prevent="addTask">
+      <input v-model="newTask" name="newTask" autocomplete="off" />
+      <button>追加する</button>
+    </form>
+    <ur>
+      <li v-for="(task, index) in tasks" :key="task">
+        <span>{{ task }}</span>
+        <button @click="$event =>deleteTask(index)">削除する</button>
+      </li>
+    </ur>
+    <button @click="allDeleteTasks">全て削除する</button>
+    <div>件数{{ tasks.length }}</div>
+    
   </div>
 </template>
+<script setup>
+  const tasks = useCookie(
+    'tasks',
+    {
+      default: () => []
+    }
+  )
+  // 新しく追加されるタスク
+  const newTask = ref('')
+  function addTask() {
+    if(newTask.value.length >= 1) {
+      tasks.value.push(newTask.value)
+    }
+    newTask.value = ''
+  }
+
+  function deleteTask(index) {
+    tasks.value.splice(index, 1)
+  }
+
+  function allDeleteTasks() {
+    tasks.value = []
+  }
+</script>
